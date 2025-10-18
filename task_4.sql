e2b1b3209964b44f80e12bc1709728fa0343e7f1
appahchristian01-crypto <appahchristian01@gmail.com>
2025-10-18 05:04:45 +0000

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,10 @@
 -- task_4.sql
--- Print the full description of the 'Books' table in alx_book_store
+-- Display full description of the 'Books' table from alx_book_store database
 -- without using DESCRIBE, EXPLAIN, or ANALYZE
 
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books'
-  AND TABLE_SCHEMA = 'alx_book_store';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
