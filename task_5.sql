09facef78952f01975677950c18efeb2b494aa43
appahchristian01-crypto <appahchristian01@gmail.com>
2025-10-18 06:07:34 +0000

    Add files via upload

---
+++ task_5.sql
@@ -0,0 +1,14 @@
+-- task_5.sql
+-- Insert a single customer into the customers table
+
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- task_5.sql
+-- Insert a single customer into the customers table
+
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
